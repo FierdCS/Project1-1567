4453ac0a5e8e922edf40e95ebdec3e4453869840
FierdCS <saw243@pitt.edu>
2025-06-19 17:48:20 -0400

    changes

--- Part 1/smoother.py
+++ Part 1/smoother.py
@@ -2,151 +2,138 @@
 
 import rospy
 import math
-from std_msgs.msg import Float32
+from std_msgs.msg import Int32MultiArray
 from geometry_msgs.msg import Twist
+from kobuki_msgs.msg import BumperEvent, Led, Sound
 
-velocityPub = rospy.publisher('/mobile_base/commands/velocity', Twist, queue_size=10)
-Led1Pub = rospy.publisher('/mobile_base/commands/led1', Led, queue_size=1)
-Led2Pub = rospy.publisher('/mobile_base/commands/led2', Led, queue_size=1)
-soundPub = rospy.publisher('/mobile_base/commands/sound', Sound, queue_size=1)
+# Publishers
+velocityPub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=10)
+Led1Pub = rospy.Publisher('/mobile_base/commands/led1', Led, queue_size=1)
+Led2Pub = rospy.Publisher('/mobile_base/commands/led2', Led, queue_size=1)
+soundPub = rospy.Publisher('/mobile_base/commands/sound', Sound, queue_size=1)
 
-
+# Globals
 command = Twist()
-
+target = Twist()
+bumperActivated = True
 emergency_brake = False
 backwards = True
-smootherMOde = 1
+smootherMode = 1
+leds = True
+onlyBackwards = False
 
-preventCollision = False
+left = 0
+right = 0
+front = 0
 
-
-def commandCallback(data):#armon wang me 
-    global bumperActivated, backwards, leds, emergency_brake, smootherMode, command
-
+def commandCallback(data):
+    global bumperActivated, backwards, leds, emergency_brake, smootherMode
     bumperActivated = bool(data.data[0])
     backwards = bool(data.data[1])
     leds = bool(data.data[2])
     emergency_brake = bool(data.data[3])
-    smootherMode = data.data[4]#0, 1, or 2
+    smootherMode = data.data[4]
 
-
-    #if not emergency brake
-
-    
-            
-
-    #else
-
-
-    
-
-def twistCallback(data):#HAS A TWIST ITEM wang me
-
-
-    if(data.linear.x >0 and preventCollision and backwards):
-        command.linear.x = 0
-        command.angular.z = 0
-    #based on mode what do we do?
-    else:
-        if smootherMode == 0:
-            command.linear.x = data.linear.x
-            command.angular.z = data.angular.z
-        elif smootherMode == 1:
-            #smoother mode, we will do a linear interpolation
-            #between the current and target velocity
-            target = data.linear.x
-            if math.fabs(target - command.linear.x) > 0.0001:
-                if target > command.linear.x:
-                    command.linear.x += 0.03
-                else:
-                    command.linear.x -= 0.03
-            command.angular.z = data.angular.z
-        
-        elif smootherMode == 2:
-            #smoother mode, we will do a linear interpolation
-            #between the current and target velocity
-            target = data.linear.x
-            if math.fabs(target - command.linear.x) > 0.0001:
-                if target > command.linear.x:
-                    command.linear.x += 0.08
-                else:
-                    command.linear.x -= 0.08
-            command.angular.z = data.angular.z
-
-
-
-    #process angular and linear
-    
-    
-    velocicityPub.publish(command)
-
+def twistCallback(data):
+    global target
+    target = data
 
 def bumperCallback(data):
-    global bumper_hit, front, left, right, preventCollision
+    global left, right, front, onlyBackwards
     if not bumperActivated:
         return
+
+    if data.bumper == 0:  # Left bumper
+        if data.state == 1:
+            left = 1
+        else:
+            left = 0
+    elif data.bumper == 1:  # Front bumper
+        if data.state == 1:
+            front = 1
+        else:
+            front = 0
+    elif data.bumper == 2:  # Right bumper
+        if data.state == 1:
+            right = 1
+        else:
+            right = 0
+
+
+    if (left + right + front) > 0:
+        emergencyBrake()
+        onlyBackwards = True
     else:
-        if data.bumper == 0:
-            if(data.state== 1):
-                left = 1
-            else:
-                left = 0
-        elif data.bumper == 1:
-            if(data.state == 1):
-                front = 1
-            else:
-                front = 0
-        elif data.bumper == 2:
-            if(data.state == 1):
-                right = 1
-            else:
-                right = 0
+        onlyBackwards = False
 
-        if(left+right+front>0):
-            emergency_brake()
-            preventCollision = True #can only back up no forward movement
+def emergencyBrake():
+    global command
+    command.linear.x = 0.0
+    command.angular.z = 0.0
+    velocityPub.publish(command)
+
+def cliffCallback(data): #may be some errors in this callback need to fix
+    global left, right, front, onlyBackwards
+
+    if data.sensor == 0:  # Left bumper
+        if data.state == 1:
+            left = 1
+        else:
+            left = 0
+    elif data.sensor == 1:  # Front bumper
+        if data.state == 1:
+            front = 1
+        else:
+            front = 0
+    else:  # Right bumper
+        if data.state == 1:
+            right = 1
+        else:
+            right = 0
 
 
-def emergencyBrake():#armon
-    global emergency_brake
-    command
-
-
-target = 0.0
-
-def driveMode():#ewang me, smoother mode
-    global target #array
-    target = data.data[4]
-
-def cliffCallback(data): # wang me
-
-def wheelDropEvent(data): #wang me armon
-
+    if (left + right + front) > 0:
+        emergencyBrake()
+        onlyBackwards = True
+    else:
+        onlyBackwards = False
+def wheelDropCallback(data):
 
 def main():
-    rospy.init_node("smoother", anonymous = True)
-    #subscribers
-    rospy.Subscribe('/robot_twist', Twist, twistCallback)
-    rospy.Subscribe('/robot_commands', Int32MultiArray, commandCallback)
-    rospy.Subscribe('/mobile_base/events/bumper', BumperEvent, bumperCallback)
-    rospy.Subscribe('/movile_base/events/cliff', CliffEvent, cliffCallback)
-    rospy.Subscribe('/mobile_base/events/wheel_drop', WheelDropEvent, wheelDropCallback)
-    rospu.spin()
-    current = 0
-    rate = rospy.Rate(100)
+    rospy.init_node("smoother", anonymous=True)
+
+    rospy.Subscriber('/robot_twist', Twist, twistCallback)
+    rospy.Subscriber('/robot_commands', Int32MultiArray, commandCallback)
+    rospy.Subscriber('/mobile_base/events/bumper', BumperEvent, bumperCallback)
+    rospy.Subscriber('/mobile_base/commands/cliff', CliffEvent, cliffCallback)
+    rospy.Subscriber('/mobile_base/commands/wheel_drop', WheelDropEvent, wheelDropCallback)
+
+    rate = rospy.Rate(100)  # 10 ms
     while not rospy.is_shutdown():
+        if target.linear.x > 0 and onlyBackwards and backwards:
+            command.linear.x = 0
+            command.angular.z = 0
+        else:
+            if smootherMode == 0:
+                command.linear.x = target.linear.x
+                command.angular.z = target.angular.z
+            elif smootherMode == 1:
+                if abs(target.linear.x - command.linear.x) > 0.0001:
+                    if target.linear.x > command.linear.x:
+                        command.linear.x += 0.03
+                    else:
+                        command.linear.x -= 0.03
+                command.angular.z = target.angular.z
+            elif smootherMode == 2:
+                if abs(target.linear.x - command.linear.x) > 0.0001:
+                    if target.linear.x > command.linear.x:
+                        command.linear.x += 0.08
+                    else:
+                        command.linear.x -= 0.08
+                command.angular.z = target.angular.z
 
-        if math.fabs(target - current) > 0.0001:
-            if target > current:
-                current += .01
-            else:
-                current -= .01
-        print(current)
+        velocityPub.publish(command)
         rate.sleep()
-
 
 if __name__ == '__main__':
     main()
-
-
-
